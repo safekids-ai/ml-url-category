a679798dbc752ede6eb10be6ed71beecf620238d
luka1618033988 <ltsab16@freeuni.edu.ge>
2023-11-04 19:14:52 +0400

    add data links to setup

--- setup.sh
+++ setup.sh
@@ -14,7 +14,9 @@ ensure_gdown_installed
 declare -A files_and_paths
 # files_and_paths["urls_link"]="./model_data/5m_urls.json"
 # files_and_paths["urls_with_cat_link"]="./model_data/5m_url_category.json"
-files_and_paths["/content/drive/MyDrive/model_data/data/quantized/model.onnx"]="./model_binary/model.onnx"
+files_and_paths["https://drive.google.com/drive/folders/1-1k9AEm-DGmD6VMcttO5skdO3_R5W414?usp=drive_link"]="./model_data/training/training_dataset"
+files_and_paths["https://drive.google.com/drive/folders/1tlPhbAruZpcoJwc2vHfjTKmM8Z5LAqZG?usp=drive_link"]="./model_data/training/test_dataset"
+files_and_paths["https://drive.google.com/file/d/1-10_d96h__tLxTdkIk0fmkZdPyBEDWQp/view?usp=drive_link"]="./model_binary/model.onnx"
 
 download_from_drive() {
     local file_id="$1"
