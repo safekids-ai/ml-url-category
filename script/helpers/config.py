a25f97713ab1bf55cb46afa48e57313c65a46d73
luka1618033988 <ltsab16@freeuni.edu.ge>
2023-11-04 19:24:07 +0400

    add batching to inference script

--- script/helpers/config.py
+++ script/helpers/config.py
@@ -4,3 +4,4 @@ ONNX_MODEL_PATH = "/model_binary/model.onnx"
 MODEL_NAME='xlm-roberta-base'
 TRAIN_DATASET_PATH = ''
 TEST_DATASET_PATH = ''
+PATH_TO_BULK_DATA = ''
--- script/infer.py
+++ script/infer.py
@@ -3,34 +3,38 @@ import numpy as np
 from transformers import AutoTokenizer
 import torch
 import pickle
-from helpers.config import ENCODER_PATH
+from helpers.config import ENCODER_PATH, PATH_TO_BULK_DATA, ONNX_MODEL_PATH
+import pandas as pd
 
 tokenizer_checkpoint = "xlm-roberta-base"
 tokenizer = AutoTokenizer.from_pretrained(tokenizer_checkpoint)
 
 def main():
     with open(ENCODER_PATH, 'rb') as f:
-            encoder = pickle.load(f)
+        encoder = pickle.load(f)
 
-    text = "Your example text goes here"
-    inputs = tokenizer(text, return_tensors="np", max_length=512, truncation=True, padding="max_length")
-    onnx_model_path = Path("/content/drive/MyDrive/model_data/data/quantized/model.onnx")
-    session = onnxruntime.InferenceSession(onnx_model_path)
-    onnx_inputs = {k: v for k, v in inputs.items() if k in [i.name for i in session.get_inputs()]}
-    outputs = session.run(None, onnx_inputs)
+    df = pd.read_parquet(PATH_TO_BULK_DATA)
+    urls = df['url'].tolist()
+    texts = list(df['text'])
+    del df
 
-    probabilities = torch.nn.functional.softmax(torch.from_numpy(outputs[0]), dim=-1)
+    batch_size = 32  # or whatever size your GPU can comfortably handle
+    predictions = []
 
-    print(probabilities.numpy())
+    session = onnxruntime.InferenceSession(ONNX_MODEL_PATH)
+    for i in range(0, len(texts), batch_size):
+            batch_texts = texts[i:i + batch_size]
+            inputs = tokenizer(batch_texts, return_tensors="np", max_length=512, 
+                            truncation=True, padding=True)
+            onnx_inputs = {k: v for k, v in inputs.items() if k in [i.name for i in session.get_inputs()]}
+            outputs = session.run(None, onnx_inputs)
+            probabilities = torch.nn.functional.softmax(torch.from_numpy(outputs[0]), dim=1)
+            predicted_indices = np.argmax(probabilities, axis=1)
+            predictions.extend(predicted_indices)
 
-
-    class_names = encoder.classes_
-
-    predicted_index = np.argmax(probabilities, axis=1)
-
-    predicted_class_name = class_names[predicted_index]
-    print(predicted_class_name)
-
+    predicted_class_names = encoder.classes_[predictions]
+    results_df = pd.DataFrame({'url': urls, 'category': predicted_class_names})
+    results_df.to_json('predicted_labels.json', orient='records', lines=True)
 
 if __name__=='__main__':
     main()
