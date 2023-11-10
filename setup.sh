6de5251a45a2682faa9eb8bacecbf9dd2d9d7e4f
luka1618033988 <ltsab16@freeuni.edu.ge>
2023-11-11 00:29:04 +0400

    replace gdown with curl

--- setup.sh
+++ setup.sh
@@ -1,31 +1,29 @@
 #!/bin/bash
 
-ensure_gdown_installed() {
-  if ! command -v gdown &> /dev/null; then
-    echo "gdown could not be found, attempting to install it."
-    pip install gdown || (echo "Failed to install gdown. Please install it manually." && exit 1)
-  else
-    echo "gdown is already installed."
-  fi
+# Function to download a file from Google Drive
+download_from_google_drive() {
+    file_id=$1
+    file_path=$2
+
+    # Check if the file already exists
+    if [ -f "$file_path" ]; then
+        echo "File $file_path already exists, skipping download."
+    else
+        # Getting the confirmation token needed for large files
+        confirm_token=$(curl -sL -c /tmp/cookies.txt "https://drive.google.com/uc?export=download&id=${file_id}" | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
+
+        # Downloading the file
+        curl -Lb /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=${confirm_token}&id=${file_id}" -o ${file_path}
+
+        # Cleaning up
+        rm -rf /tmp/cookies.txt
+        echo "Downloaded $file_path."
+    fi
 }
 
-ensure_gdown_installed
+# Download files
+download_from_google_drive "1-10_d96h__tLxTdkIk0fmkZdPyBEDWQp" "./model_binary/model.onnx"
+download_from_google_drive "1r1rNR6lJRu_Wk3zQpQxoogS7o0u9FxJ1" "./model_binary/encoder.pkl"
 
-declare -A files_and_paths
-# files_and_paths["urls_link"]="./model_data/5m_urls.json"
-# files_and_paths["urls_with_cat_link"]="./model_data/5m_url_category.json"
-files_and_paths["https://drive.google.com/drive/folders/1-1k9AEm-DGmD6VMcttO5skdO3_R5W414?usp=drive_link"]="./model_data/training/training_dataset"
-files_and_paths["https://drive.google.com/drive/folders/1tlPhbAruZpcoJwc2vHfjTKmM8Z5LAqZG?usp=drive_link"]="./model_data/training/test_dataset"
-files_and_paths["https://drive.google.com/file/d/1-10_d96h__tLxTdkIk0fmkZdPyBEDWQp/view?usp=drive_link"]="./model_binary/model.onnx"
 
-download_from_drive() {
-    local file_id="$1"
-    local destination_path="$2"
-    gdown --id "$file_id" --output "$destination_path"
-}
-
-for file_id in "${!files_and_paths[@]}"; do
-    download_from_drive "$file_id" "${files_and_paths[$file_id]}"
-done
-
-echo "All files have been downloaded."
+echo "All file download attempts completed."
