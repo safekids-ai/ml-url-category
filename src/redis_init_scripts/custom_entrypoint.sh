566f284b5fcbfdce6855e041709ee0c7bc1a2b5e
luka1618033988 <ltsab16@freeuni.edu.ge>
2023-11-19 02:10:52 +0400

    minor changes

--- src/redis_init_scripts/custom_entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# custom_entrypoint.sh
-
-# Run the data fetching and populating script
-python3 /usr/local/bin/fetch_and_populate.py
-
-# Call the original Redis entrypoint
-exec docker-entrypoint.sh redis-server "$@"
