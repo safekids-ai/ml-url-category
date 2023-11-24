e841ea400027082e37078789f91270c8e43f7519
luka1618033988 <ltsab16@freeuni.edu.ge>
2023-11-24 21:02:58 +0400

    update scraper with new html parser

--- src/web_scraper/helpers/utils.py
+++ src/web_scraper/helpers/utils.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import requests
 import re
+from bs4.element import Comment
 from bs4 import BeautifulSoup
 import os
+import json
 import pyarrow as pa
 import pyarrow.parquet as pq
 from io import BytesIO
-from helpers.config import PROTOCOLS, LOCAL_TMP_DIR, LOCAL_TMP_DIR_ZIP, headers
+from helpers.config import PROTOCOLS, LOCAL_TMP_DIR, LOCAL_TMP_DIR_ZIP, LOCAL_TMP_TXT_PATH, headers
 import zipfile
 
 def write_zip(zip_filename,txts,client_or_none,bucket_name):
@@ -17,6 +19,14 @@ def write_zip(zip_filename,txts,client_or_none,bucket_name):
         client_or_none.upload_file(f'{LOCAL_TMP_DIR_ZIP}/' + zip_filename, bucket_name, zip_filename)
         os.remove(f'{LOCAL_TMP_DIR_ZIP}/'+zip_filename)
     
+def write_parquet(uuid, client_or_none, bucket_or_directory,mode):
+    with open(LOCAL_TMP_TXT_PATH, encoding="utf-8") as f:
+        dict_list = [json.loads(line.strip()) for line in f.readlines()]
+    # Creating a DataFrame from the list of dictionaries
+    df = pd.DataFrame(dict_list)
+    os.remove(LOCAL_TMP_TXT_PATH)
+    write_df(df, bucket_or_directory, f'{uuid}.parquet', client_or_none, mode)
+
 #retrieval function which will be called by ThreadPoolExecutor
 # def retrieve(url):
 #     def process_resp(response):
@@ -38,15 +48,66 @@ def write_zip(zip_filename,txts,client_or_none,bucket_name):
 #     except:
 #         pass
 
+
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
+
+
+def extract_standard_tags(response,url):
+    html = response.content
+    soup = BeautifulSoup(html, 'html.parser')
+
+    title = soup.title.string if soup.title else None
+    meta_tags = [{'name': tag.get('name'), 'content': tag.get('content')} for tag in soup.find_all('meta')]
+    headings = {f'h{i}': [tag.string for tag in soup.find_all(f'h{i}')] for i in range(1, 7)}
+    images = [{'src': tag['src'], 'alt': tag.get('alt', '')} for tag in soup.find_all('img') if tag.get('src')]
+    links = [tag['href'] for tag in soup.find_all('a') if tag.get('href')]
+    paragraphs = [tag.get_text() for tag in soup.find_all('p')]
+    list_items = [tag.get_text() for tag in soup.find_all('li')]
+    bold_texts = [tag.get_text() for tag in soup.find_all(['b', 'strong'])]
+    # italic_texts = [tag.get_text() for tag in soup.find_all(['i', 'em'])]
+    # blockquotes = [tag.get_text() for tag in soup.find_all('blockquote')]
+    # comments = [str(comment) for comment in soup.find_all(string=lambda text: isinstance(text, Comment))]
+
+    texts = soup.findAll(text=True)
+    visible_texts = filter(tag_visible, texts)
+    visible_text = u" ".join(t.strip() for t in visible_texts)
+    
+    return {'url': url,
+        'title': title,
+        'meta_tags': meta_tags,
+        'headings': headings,
+        'images': images,
+        'links': links,
+        'paragraphs': paragraphs,
+        'list_items': list_items,
+        'bold_texts': bold_texts,
+        # 'italic_texts': italic_texts,
+        # 'blockquotes': blockquotes,
+        # 'comments': comments,
+        'visible_text': visible_text
+    }
+
+def append_to_tmp_txt(tmp_txt_path,url_dict):
+    with open(tmp_txt_path, 'a',encoding="utf-8") as file:
+        json.dump(url_dict, file)
+        file.write("\n")
+
 def retrieve(url):
     try:
         # Try both HTTP and HTTPS protocols to retrieve the content
         for protocol in PROTOCOLS:
             r = requests.get(protocol+url,headers=headers, timeout=5)
             if r.status_code == 200:
-#                 text = process_resp(r)
-                with open(f'{LOCAL_TMP_DIR}/{url}.html', "w", encoding="utf-8") as file:
-                    file.write(r.text)
+                data = extract_standard_tags(r)
+                append_to_tmp_txt(LOCAL_TMP_TXT_PATH, data)
+                # with open(f'{LOCAL_TMP_DIR}/{url}.html', "w", encoding="utf-8") as file:
+                #     file.write(r.text)
+                
             break
     except:
         pass
